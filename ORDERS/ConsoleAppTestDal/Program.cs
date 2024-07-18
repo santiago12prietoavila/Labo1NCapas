3e3a3ad42e609a297d46d557a9a6368f428d7215
santiago12prietoavila <santiagoprietoavila943@gmail.com>
2024-07-17 21:50:58 -0500

    Feat(test:Add layer and ConsoleAppTest)

---
+++ ORDERS/ConsoleAppTestDal/Program.cs
@@ -0,0 +1,57 @@
+﻿using DAL;
+using Entities.Models;
+using System.Linq.Expressions;
+
+//CreateAsync().GetAwaiter().GetResult();
+RetreiveAsync().GetAwaiter().GetResult();
+static async Task CreateAsync()
+{
+    //Add Customer
+    Customer customer = new Customer()
+    {
+        FirstName = "Miguel",
+        LastName = "Gomez",
+        City = "Bogotá",
+        Country = "Colombia",
+        Phone = "3125594627"
+
+    };
+
+    using (var repository = RepositoryFactory.CreateRepository())
+    {
+        try
+        {
+            var createdCustomer = await repository.CreateAsync(customer);
+            Console.WriteLine($"Added Customer: {createdCustomer.FirstName} {createdCustomer.LastName}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error {ex.Message}");
+        }
+
+
+    }
+
+}
+
+static async Task RetrieveAsync()
+{
+    using (var repository = RepositoryFactory.CreateRepository())
+    {
+        try
+        {
+            Expression<Func<Customer, bool>> criteria = c => c.FirstName == "Andres" && c.LastName == "Pinzon";
+            var customer = await repository.RetrieveAsync(criteria);
+            if (customer != null)
+            {
+                Console.WriteLine($"Retrived customer: {customer.FirstName} \t{customer.LastName}\t City: {customer.City}\t Country: {customer.Country}");
+            }
+            Console.WriteLine($"Customer not exist");
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
